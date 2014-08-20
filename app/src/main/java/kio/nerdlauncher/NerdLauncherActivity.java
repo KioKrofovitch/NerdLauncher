adef4725ec1a83ae205cc2ed463fb7084dd7bf22
KioKrofovitch <kio.krofovitch@gmail.com>
2014-08-20 15:49:24 -0600

    Initial commit

---
+++ app/src/main/java/kio/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,12 @@
+package kio.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return new NerdLauncherFragment();
+    }
+}
---
+++ app/src/main/java/kio/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,7 @@
+package kio.nerdlauncher;
+
+import android.support.v4.app.ListFragment;
+
+public class NerdLauncherFragment extends ListFragment {
+    
+}
